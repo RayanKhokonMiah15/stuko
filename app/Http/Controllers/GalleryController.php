c5b6de043726fd1c1f0026a7768c5aa3620650f9
RyuuKanaeru <firdausryan.aa@gmail.com>
2025-04-25 14:49:56 +0700

    baru mau buat create nih

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -23,6 +23,7 @@ class GalleryController extends Controller
     public function create()
     {
         //
+        return view ('gallery.create');
     }
 
     /**
---
+++ resources/views/gallery/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+<form action ="{{route('gallery.store')}}" method="post">
+    @csrf
+    <div class="col-12">
+        <div class="card shadow mb-4">
+        <div class="card-header py-3 ">
+            <h6>Lu mau tambah data? Di sini Isinya</h6>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label>Nama Foto</label>
+                    <input type="text" class="form-control" name="nama_foto" value="{{old('nama_foto')}}">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button> sampai sini gua </button>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+@endsection
